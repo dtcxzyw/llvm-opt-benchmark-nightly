0806344cdbe5008ac235ae64a78008ddc7eae46d
Yingwei Zheng <dtcxzyw2333@gmail.com>
2026-07-06 15:50:20 +0000

    chore: Adjust stockfish compilation flags
    Co-authored-by: Disservin <disservin.social@gmail.com>

--- repo/stockfish/build.clang.sh
+++ repo/stockfish/build.clang.sh
@@ -1,4 +1,4 @@
 set -eo pipefail
 git clone --branch sf_18 --depth 1 https://github.com/official-stockfish/Stockfish.git
 cd Stockfish/src
-make -j build ARCH=x86-64-avx512icl COMP=clang COMPCXX="$CXX"
+make -j build COMP=clang COMPCXX="$CXX" EXTRACXXFLAGS="-march=znver5"
