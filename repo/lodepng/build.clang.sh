dfff02d9f75857e5fadcc8bea2b864518a2e5026
Yingwei Zheng <dtcxzyw2333@gmail.com>
2026-08-16 18:31:59 +0800

    chore: Add lodepng

---
+++ repo/lodepng/build.clang.sh
@@ -0,0 +1,7 @@
+set -eo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+git clone --branch master --depth 1 https://github.com/lvandeve/lodepng.git
+cp $DIR/CMakeLists.txt CMakeLists.txt
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -G Ninja
+cmake --build . -j
